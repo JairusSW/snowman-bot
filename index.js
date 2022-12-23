8edda253d678f74af0ce45206134b8c7b2f4c36b
Silas <silasschlax@gmail.com>
2022-12-23 14:55:34 -0500

    add: musicb and index.js edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const ReziDB = require("rezidb");
-const cleverbot = require('cleverbot-free');
+const cleverbot = require("cleverbot-free");
 //const Chatbot = require("discord-chatbot")
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 require("dotenv").config({ path: "./config.env" });
@@ -11,42 +11,38 @@ const adminIDs = [600700584038760448, 890576956054188083];
 
 // If you run bot with -w or --watch it will never die and you can never stop it.
 if (process.argv.includes("-w") || process.argv.includes("--watch")) {
-    process.on("exit", function () {
-        console.log("Bot crashed. Restarting...");
-        child_process.spawn(
-            process.argv.shift(),
-            process.argv,
-            {
-                cwd: process.cwd(),
-                detached: false,
-                stdio: "inherit"
-            }
-        );
+  process.on("exit", function () {
+    console.log("Bot crashed. Restarting...");
+    child_process.spawn(process.argv.shift(), process.argv, {
+      cwd: process.cwd(),
+      detached: false,
+      stdio: "inherit",
     });
+  });
 }
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.AutoModerationConfiguration,
-        GatewayIntentBits.AutoModerationExecution,
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.DirectMessageTyping,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildEmojisAndStickers,
-        GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildScheduledEvents,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildWebhooks,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.MessageContent,
-    ],
+  intents: [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.prefix = process.env.prefix;
@@ -54,127 +50,128 @@ client.prefix = process.env.prefix;
 client.conversation = [];
 
 client.db = {
-    guilds: new ReziDB({
-        name: "guilds",
-        path: "./database",
-        cluster: false,
-        cache: true,
-    }),
-    users: new ReziDB({
-        name: "users",
-        path: "./database",
-        cluster: false,
-        cache: true,
-    }),
+  guilds: new ReziDB({
+    name: "guilds",
+    path: "./database",
+    cluster: false,
+    cache: true,
+  }),
+  users: new ReziDB({
+    name: "users",
+    path: "./database",
+    cluster: false,
+    cache: true,
+  }),
 };
 
 client.hangman = new Map();
 
 client.commands = new Collection();
 const commands = fs
-    .readdirSync("./src/commands")
-    .filter((file) => file.endsWith(".js"));
+  .readdirSync("./src/commands")
+  .filter((file) => file.endsWith(".js"));
 const events = fs
-    .readdirSync("./src/events")
-    .filter((file) => file.endsWith(".js"));
+  .readdirSync("./src/events")
+  .filter((file) => file.endsWith(".js"));
 for (const file of commands) {
-    const command = require(`./src/commands/${file}`);
-    console.log(`Attempting to load command ${command.name}`);
-    client.commands.set(command.name, command);
+  const command = require(`./src/commands/${file}`);
+  console.log(`Attempting to load command ${command.name}`);
+  client.commands.set(command.name, command);
 }
 
 for (const ev of events) {
-    const eventName = ev.split(".")[0];
-    const eventFunc = require(`./src/events/${ev}`);
-    console.log(`Attempting to load event ${eventName}`);
-    if (!eventFunc.once) {
-        client.on(eventName, (a, b, c) => {
-            eventFunc.execute(client, a, b, c);
-        });
-    } else {
-        client.once(eventName, (a, b, c) => {
-            eventFunc.execute(client, a, b, c);
-        });
-    }
+  const eventName = ev.split(".")[0];
+  const eventFunc = require(`./src/events/${ev}`);
+  console.log(`Attempting to load event ${eventName}`);
+  if (!eventFunc.once) {
+    client.on(eventName, (a, b, c) => {
+      eventFunc.execute(client, a, b, c);
+    });
+  } else {
+    client.once(eventName, (a, b, c) => {
+      eventFunc.execute(client, a, b, c);
+    });
+  }
 }
 
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    // Inject code into messageCreate for hangman utility. Maybe make a wrapper for this later?
-    if (client.hangman.size) {
-        for (let i = 0; i < client.hangman.size; i++) {
-            if (client.hangman.has(message.author.id)) {
-                client.hangman.get(message.author.id)(message.content.trim().toLowerCase());
-                message.delete();
-            }
-        }
+  if (message.author.bot) return;
+  // Inject code into messageCreate for hangman utility. Maybe make a wrapper for this later?
+  if (client.hangman.size) {
+    for (let i = 0; i < client.hangman.size; i++) {
+      if (client.hangman.has(message.author.id)) {
+        client.hangman.get(message.author.id)(
+          message.content.trim().toLowerCase()
+        );
+        message.delete();
+      }
     }
+  }
 
-    let guildData = await client.db.guilds.get(message.guild.id);
+  let guildData = await client.db.guilds.get(message.guild.id);
 
-    // Default props of guilds
-    if (!guildData) {
-        guildData = {
-            prefix: process.env.prefix,
-            starboardChannel: "starboard"
-        };
-        await client.db.guilds.set(message.guild.id, guildData);
+  // Default props of guilds
+  if (!guildData) {
+    guildData = {
+      prefix: process.env.prefix,
+      starboardChannel: "starboard",
+    };
+    await client.db.guilds.set(message.guild.id, guildData);
+  }
+
+  const userData = await client.db.users.get(
+    message.author.id + message.guild.id
+  );
+  console.log("Prefix: " + guildData.prefix);
+  if (!message.content.startsWith(guildData.prefix)) {
+    if (userData) {
+      userData.stats.messages.sent++;
+      await client.db.users.set(message.author.id + message.guild.id, userData);
     }
-
-    const userData = await client.db.users.get(message.author.id);
-
-    console.log("Prefix: " + guildData.prefix)
-    if (!message.content.startsWith(guildData.prefix)) {
-        if (userData) {
-            userData.stats.messages.sent++;
-            await client.db.users.set(message.author.id, userData);
-        }
-        return;
+    return;
+  }
+  const args = message.content.slice(1).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (client.commands.has(command)) {
+    const commandFunc = client.commands.get(command);
+    if (userData) {
+      userData.stats.commands.sent++;
+      await client.db.users.set(message.author.id + message.guild.id, userData);
     }
-    const args = message.content
-        .slice(1)
-        .trim()
-        .split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if (client.commands.has(command)) {
-        const commandFunc = client.commands.get(command);
-        if (userData) {
-            userData.stats.commands.sent++;
-            await client.db.users.set(message.author.id, userData);
-        }
-        if (commandFunc.isAdmin && !adminIDs.includes(message.author.id)) {
-            message.channel.send("You lowly weeb. You can't do this boi. :(((((");
-        } else {
-            commandFunc.execute(message, args);
-        }
+    if (commandFunc.isAdmin && !adminIDs.includes(message.author.id)) {
+      message.channel.send("You lowly weeb. You can't do this boi. :(((((");
+    } else {
+      commandFunc.execute(message, args);
     }
+  }
 });
 
 client.login(process.env.token);
 
 let lastMessage = 0;
 
-client.on("messageCreate", message => {
-    if (message.author.bot) return;
-    console.log(message.content)
-    if (message.content.trimStart().startsWith(client.prefix)) return;
-    if (client.hangman.has(message.author.id)) return;
-    if (!message.content.trimStart().startsWith(`<@${client.user.id}>`)) return;
-    if (((Date.now() - lastMessage) < 5000) && lastMessage !== 0) {
-        lastMessage = Date.now();
-        message.channel.send(`<@${message.author.id}>, Slow down!`);
-        return;
-    }
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  console.log(message.content);
+  if (message.content.trimStart().startsWith(client.prefix)) return;
+  if (client.hangman.has(message.author.id)) return;
+  if (!message.content.trimStart().startsWith(`<@${client.user.id}>`)) return;
+  if (Date.now() - lastMessage < 5000 && lastMessage !== 0) {
     lastMessage = Date.now();
-    let text = message.content.slice(message.content.indexOf(">") + 1 || 0);
+    message.channel.send(`<@${message.author.id}>, Slow down!`);
+    return;
+  }
+  lastMessage = Date.now();
+  let text = message.content.slice(message.content.indexOf(">") + 1 || 0);
 
-    cleverbot(text, client.conversations).then(res => {
-        client.conversation.push(text);
-        client.conversation.push(res);
-        client.conversation = client.conversation.slice(0, 10);
-        message.channel.send(`<@${message.author.id}>, ${res}`);
-
-    }).catch(err => {
-        console.log(`The AI could not compute`);
+  cleverbot(text, client.conversations)
+    .then((res) => {
+      client.conversation.push(text);
+      client.conversation.push(res);
+      client.conversation = client.conversation.slice(0, 10);
+      message.channel.send(`<@${message.author.id}>, ${res}`);
+    })
+    .catch((err) => {
+      console.log(`The AI could not compute`);
     });
-})
+});
---
+++ src/commands/play.js
@@ -0,0 +1,48 @@
+const { EmbedBuilder } = require("discord.js");
+const play = require("play-dl");
+const {
+  createAudioPlayer,
+  NoSubscriberBehavior,
+  createAudioResource,
+  joinVoiceChannel,
+  VoiceConnectionStatus,
+  AudioPlayerStatus,
+  getVoiceConnection,
+} = require("@discordjs/voice");
+const fs = require("fs");
+module.exports = {
+  name: "play",
+  description: "View the database as a whole",
+  guildOnly: true,
+  cooldown: 3,
+  async execute(message, args) {
+    const { channel } = message.member.voice;
+
+    if (!channel) {
+      message.channel.send("Enter a voice channel");
+    }
+
+    if (!message.client.voiceConnection)
+      message.client.voiceConnection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+
+    const stream = await play.stream(args[0]);
+
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type,
+    });
+    if (message.client.voicePlayer) {
+      client.voicePlayer.stop();
+    }
+    message.client.voicePlayer = createAudioPlayer({
+      behaviors: {
+        noSubscriber: NoSubscriberBehavior.Play,
+      },
+    });
+    message.client.voicePlayer.play(resource);
+    message.client.voiceConnection.subscribe(message.client.voicePlayer);
+  },
+};
